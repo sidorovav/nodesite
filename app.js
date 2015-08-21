028dc41592f68ab9fcac9f84c9eacd74723f8100
Alexandr V Sidorov <sidorovav@gmail.com>
2015-08-21 12:59:54 +0300

    Hello world added

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /**
  * Created by softmaster on 21.08.2015.
  */
+'use strict';
+var http = require('http')
+    , express = require('express');
+var app = express();
+app.get('/',function(req,res){
+   res.send('Hello world!');
+});
+http.createServer(app).listen(8123, function() {
+    console.log('Visit http://localhost:8123 to begin your work.');
+});
